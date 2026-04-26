7e3fde06c6db62cba60bc96a43ab8a5e6e550c6e
chelsearwang <109623274+chelsearwang@users.noreply.github.com>
2026-04-25 23:59:05 -0700

    lobby router creater

--- client/src/components/LobbyRouterCreater.tsx
+++ client/src/components/LobbyRouterCreater.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useRuns } from "../context/RunContext";
 import type { RunGroup, RunRoute } from "../types/runTypes";
-import { route1, route3 } from "../services/routes";
+import { route1, route2, route3, route4, route5, route6, route7, route8, route9, route10, route11, route12 } from "../services/routes";
 
 const LobbyRouterCreater = () => {
   const { publicRuns, addRoute, addRun } = useRuns();
@@ -30,19 +30,104 @@ const LobbyRouterCreater = () => {
       isPrivate: false,
       status: "open",
     } as RunGroup);
+    addRun({
+      id: 3,
+      creatorId: 124356789,
+      startTime: new Date(),
+      routeId: 3,
+      targetPace: 8.30,
+      maxPlayers: 10,
+      playerIds: [12, 14, 22, 67, 76],
+      isPrivate: false,
+      status: "open",
+    } as RunGroup);
+    addRun({
+      id: 4,
+      creatorId: 124356789,
+      startTime: new Date(),
+      routeId: 4,
+      targetPace: 9.30,
+      maxPlayers: 10,
+      playerIds: [11, 21, 25, 68, 80],
+      isPrivate: false,
+      status: "open",
+    } as RunGroup);
+    addRun({
+      id: 5,
+      creatorId: 124356789,
+      startTime: new Date(),
+      routeId: 5,
+      targetPace: 10,
+      maxPlayers: 10,
+      playerIds: [1, 12, 23, 44],
+      isPrivate: false,
+      status: "open",
+    } as RunGroup);
+    addRun({
+      id: 6,
+      creatorId: 124356789,
+      startTime: new Date(),
+      routeId: 6,
+      targetPace: 7.5,
+      maxPlayers: 10,
+      playerIds: [2, 4, 13, 26, 37, 46, 55, 64, 92],
+      isPrivate: false,
+      status: "open",
+    } as RunGroup);
+    addRun({
+      id: 7,
+      creatorId: 124356789,
+      startTime: new Date(),
+      routeId: 7,
+      targetPace: 8,
+      maxPlayers: 10,
+      playerIds: [5, 7, 11, 28, 39, 40, 52],
+      isPrivate: false,
+      status: "open",
+    } as RunGroup);
   }, [publicRuns]);
   useEffect(() => {
     addRoute({
       id: 1,
-      name: "Morning 5K Run",
-      route: route3,
-      distance: 5,
+      name: "Morning Run to Beach",
+      route: route1,
+      distance: 6,
     } as RunRoute);
     addRoute({
       id: 2,
-      name: "Evening Trail Run",
+      name: "Evening Run to Beach",
       route: route1,
-      distance: 2,
+      distance: 6,
+    } as RunRoute);
+    addRoute({
+      id: 3,
+      name: "Evening Trail Run",
+      route: route2,
+      distance: 6.4,
+    } as RunRoute);
+    addRoute({
+      id: 4,
+      name: "Morning Park Run",
+      route: route2,
+      distance: 6.4,
+    } as RunRoute);
+    addRoute({
+      id: 5,
+      name: "Morning Run to Santa Monica",
+      route: route3,
+      distance: 9.6,
+    } as RunRoute);
+    addRoute({
+      id: 6,
+      name: "Afternoon Run to Sawtelle",
+      route: route4,
+      distance: 4.8,
+    } as RunRoute);
+    addRoute({
+      id: 6,
+      name: "UCLA Campus Loop",
+      route: route7,
+      distance: 4.1,
     } as RunRoute);
   }, []);
 
